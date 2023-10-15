d72fa56c48aebc3d4bb8c69d23cd01c9bb1e00dc
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-15 15:31:21 +0530

    added audio logic

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click" , handleClick);
+}
+
+function handleClick() {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+}
+
