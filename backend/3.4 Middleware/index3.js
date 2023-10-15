c2d14a0c6fc0fae927af55927b27cf0499babd6b
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-15 23:21:30 +0530

    middleware completed

--- backend/3.4 Middleware/index3.js
+++ backend/3.4 Middleware/index3.js
@@ -3,7 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+
+
+app.use((req,res,next) => {
+  console.log("request : "+req.method);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
