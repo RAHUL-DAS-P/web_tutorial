146165d7302ac3cbd7dcdfdb4b52460eadac777e
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-15 18:01:02 +0530

    drums project completed

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,11 +6,21 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 function handleClick() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 }
 
 document.addEventListener("keypress", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function()  {
+        activeButton.classList.remove("pressed");  
+    }, 1000);
+}
 
 function makeSound(key) {
     switch (key) {
