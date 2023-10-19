c4e4feae598ad7babe7ef0ca1a4ce8f0a948022f
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-19 22:37:33 +0530

    the project is completed

--- API_project/rest_api/solution.js
+++ API_project/rest_api/solution.js
@@ -3,11 +3,11 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "c9ca9431-b31c-417d-8235-561fe0f7afe8";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
