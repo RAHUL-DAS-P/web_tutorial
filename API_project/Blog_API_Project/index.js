c3762b10d2e903f5910ef4a1077631c29c6db13f
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-21 00:16:22 +0530

    PATCHAND DELETE DONE

--- API_project/Blog_API_Project/index.js
+++ API_project/Blog_API_Project/index.js
@@ -77,9 +77,36 @@ app.post("/posts/", (req, res) => {
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
+app.patch("/posts/:id", (req, res) => {
+  try {
+    const postId = parseInt(req.params.id);
+    const post = posts.find((p) => p.id === postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (req.body.title) post.title = req.body.title;
+    if (req.body.content) post.content = req.body.content;
+    if (req.body.author) post.author = req.body.author;
+
+    res.json(post);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+
+app.delete("/posts/:id", (req, res) => {
+  const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+
+  posts.splice(index, 1);
+  res.json({ message: "Post deleted" });
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
