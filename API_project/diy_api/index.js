5ef1b06abe3bde4e3f97c990c34a4cc052ca6277
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-20 23:40:36 +0530

    delete all and project done

--- API_project/diy_api/index.js
+++ API_project/diy_api/index.js
@@ -98,6 +98,19 @@ app.delete("/delete/:id", (req, res) => {
 
 //8. DELETE All jokes
 
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
 });
